267926455262e10c89f357227a84335e8e2a34d4
laque <eduardo.laquintana13@imperial.ac.uk>
2017-11-29 16:47:34 +0100

    comment message to test

--- app/src/main/java/com/tlc/laque/redcarpet/MainActivity.java
+++ app/src/main/java/com/tlc/laque/redcarpet/MainActivity.java
@@ -40,6 +40,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        
+        //TODO Comment code properly
     }
 
     @Override
